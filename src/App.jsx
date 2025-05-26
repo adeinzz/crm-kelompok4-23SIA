1e34f8654fa71aac78577f3b14e2f4e2c553bf07
Dafiz <dafiz23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:38 +0700

    ProductManajement sudah ok by dafiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import viteLogo from '/vite.svg'
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import ProductManagement from './pages/ProductManagement'
+
 
 function App() {
 
@@ -12,6 +14,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />} >
         <Route path="/" element={<Dashboard /> } />
+        <Route path="/produk" element={<ProductManagement /> } />
         </Route>
       </Routes>
   )
