ea51e056b4c563eb00549809ee8589fbfbe04ed7
Ranny <nasrani23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:26 +0700

    penjualan sudah okey by Ranny

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import SalesManagement from './pages/SalesManagement'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />} >
         <Route path="/" element={<Dashboard /> } />
+        <Route path="/penjualan" element={<SalesManagement /> } />
         </Route>
       </Routes>
   )
