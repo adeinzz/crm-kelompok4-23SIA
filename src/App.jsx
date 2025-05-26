10008bcbcaca5d8d4b622318924a193c638b9d27
Lidya <lidya23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:23 +0700

    pelanggan sudah oke by ldy

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import CustomerManagement from './pages/CustomerManagement'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />} >
         <Route path="/" element={<Dashboard /> } />
+        <Route path="/customer" element={<CustomerManagement /> } />
         </Route>
       </Routes>
   )
